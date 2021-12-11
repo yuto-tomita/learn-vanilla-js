2dd40b304dbf14d0554c058f919cd4cb0c2efe40
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-12-12 00:09:47 +0900

    react版TODOを作成

--- compare/src/App.tsx
+++ compare/src/App.tsx
@@ -1,17 +1,81 @@
 import { useState } from 'react'
 
+interface Todo {
+  isEditMode: boolean
+  todoValue: string
+}
+
 function App() {
-  const [todo, setTodo] = useState<string[]>([])
+  const [todo, setTodo] = useState<Todo[]>([])
   const [todoValue, setTodoValue] = useState('')
 
   const addTodo = () => {
-    setTodo([...todo, todoValue])
+    setTodo([...todo, {isEditMode: false, todoValue}])
     setTodoValue('')
   }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTodoValue(e.target.value)
+  const deleteTodo = (key: number) => {
+    const filterTodo = todo.filter((_, num) => num !== key)
+    setTodo(filterTodo)
   }
+
+  const changeMode = (key: number) => {
+    const updateMode = todo.map((val, index) => {
+      if (index === key) {
+        return {...val, isEditMode: true}
+      } else {
+        return {...val}
+      }
+    })
+
+    setTodo(updateMode)
+  }
+
+  const updateTodo = (key: number) => {
+    const updateValueTodoList = todo.map((val, index) => {
+      if (index === key) {
+        return {
+          isEditMode: false,
+          todoValue
+        } 
+      } else {
+        return {...val}
+      }
+    })
+    setTodo(updateValueTodoList)
+  }
+
+  const todoState = () => {
+    return todo.map((val, key) => {
+      if (val.isEditMode) {
+        return (
+          <li>
+            <input
+              key={key}
+              value={val.todoValue}
+              onChange={(event) => setTodoValue(event.target.value)}
+            />
+            <button onClick={() => updateTodo(key)}>
+              update
+            </button>
+          </li>
+        )
+      } else {
+        return (
+          <li
+            key={key}
+            onClick={() => changeMode(key)}
+          >
+            {val.todoValue}
+            <button onClick={() => deleteTodo(key)}>
+              delete
+            </button>
+          </li>
+        )
+      }
+    })
+  }
+
 
   return (
     <div>
@@ -22,13 +86,13 @@ function App() {
         <input
           type="text"
           value={todoValue}
-          onChange={(event) => handleChange(event)}
+          onChange={(event) => setTodoValue(event.target.value)}
         />
         <button onClick={() => addTodo()}>Add item</button>
       </div>
 
       <ul>
-        {todo.map((val, key) => <li key={key}>{val}</li>)}
+        {todoState()}
       </ul>
     </div>
   )
