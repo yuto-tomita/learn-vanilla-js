15a3b4916fb0300049abaf32790b9196df16705d
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2021-12-11 19:56:36 +0900

    TODOが投稿できるように加筆

--- compare/src/App.tsx
+++ compare/src/App.tsx
@@ -1,50 +1,35 @@
 import { useState } from 'react'
-import logo from './logo.png'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState<string[]>([])
+  const [todoValue, setTodoValue] = useState('')
+
+  const addTodo = () => {
+    setTodo([...todo, todoValue])
+    setTodoValue('')
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value)
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
+    <div>
+      <h1>todo list</h1>
+
+      <div>
+        <label>Enter a new item:</label>
+        <input
+          type="text"
+          value={todoValue}
+          onChange={(event) => handleChange(event)}
         />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button
-            type="button"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+        <button onClick={() => addTodo()}>Add item</button>
+      </div>
+
+      <ul>
+        {todo.map((val, key) => <li key={key}>{val}</li>)}
+      </ul>
     </div>
   )
 }
